e9cb5ddb4ffe512a8d59d5df6d8369743246b5ab
muhammad-talib <muhammadtalib@folio3.com>
2023-12-26 16:19:32 +0500

    Built Navigation Bar

--- news-app/src/App.tsx
+++ news-app/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import NavigationBar from "./Components/NavigationBar";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
 }
 
 export default App;
